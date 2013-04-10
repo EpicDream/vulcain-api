30ca617417e4536769000ab648cd0cdc94600e83
anoiaque <anoiaque@gmail.com>
2013-04-10 18:15:23 +0200

    * Answers Controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,10 +1,10 @@
-class PaymentsController < ApplicationController
+class AnswersController < ApplicationController
   SESSION_KEYS = ['uuid', 'callback_url', 'state']
+  ANSWERS_KEYS = ['question_id', 'answer']
   
   before_filter :set_context
   
   def create
-    puts params.inspect
     unless check_parameters
       render :json => {:error => "Missing or Bad parameters"}.to_json, :status => 451
     else
@@ -15,20 +15,21 @@ class PaymentsController < ApplicationController
   private
   
   def set_context
-    @context = JSON.parse(params['context']) if params['context']
+    @context = params['context']
   end
   
   def check_parameters
     @context &&
-    @context['response'] &&
-    assert_keys(@context['session'].keys, SESSION_KEYS)
+    @context['answers'] &&
+    @context['answers'].count > 0 &&
+    assert_keys(@context['session'].keys, SESSION_KEYS) &&
+    @context['answers'].inject(true) { |valid, answer| valid && assert_keys(answer.keys, ANSWERS_KEYS)  }
   rescue
     false
   end
   
   def message
-    { :verb => :response, 
-      :vendor => "RueDuCommerce",
+    { :verb => :answer, 
       :context => @context
     }.to_json
   end
