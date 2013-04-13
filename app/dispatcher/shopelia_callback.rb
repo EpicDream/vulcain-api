f5dffc316451c6984061a48969a021757225af69
Vulcain <vulcain@shopelia.fr>
2013-04-13 23:39:10 +0200

    Add json headers

--- app/dispatcher/shopelia_callback.rb
+++ app/dispatcher/shopelia_callback.rb
@@ -5,6 +5,8 @@ class ShopeliaCallback
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Put.new(uri.request_uri)
     request.body = data.to_json
+    request.add_field "Content-type", "application/json"
+    request.add_field "Accept", "application/json"
     http.request(request)
   end
   
