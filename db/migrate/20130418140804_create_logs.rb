c23a3d4792cfa7e81b71e5a29885fb467f6aff82
anoiaque <anoiaque@gmail.com>
2013-04-18 16:49:12 +0200

    change column name
    + handling json via AR , only with rails 4 ...

--- db/migrate/20130418140804_create_logs.rb
+++ db/migrate/20130418140804_create_logs.rb
@@ -1,7 +1,7 @@
 class CreateLogs < ActiveRecord::Migration
   def change
     create_table :logs, :force => true do |t|
-      t.text :json
+      t.text :payload
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(:version => 20130418140804) do
 
   create_table "logs", :force => true do |t|
-    t.text     "json"
+    t.text     "payload"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
