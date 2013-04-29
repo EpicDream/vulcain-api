b44df24697020c8e949dc4b4cd096621f856312e
Vincent Renaudineau <vincent.renaudineau@prixing.fr>
2013-04-29 17:05:58 +0200

    Fix in background.

--- plugin/mapper/background.js
+++ plugin/mapper/background.js
@@ -1,55 +1,47 @@
+var activeHost = JSON.parse(localStorage.activeHost || "{}");
 var activeTabs = {};
-var inactiveHost = [];
 
-chrome.runtime.onInstalled.addListener(function() {
+chrome.extension.onMessage.addListener(function(request, sender) {
+  chrome.tabs.sendMessage(sender.tab.id, request);
+});
 
-  chrome.extension.onMessage.addListener(function(request, sender) {
-    chrome.tabs.sendMessage(sender.tab.id, request);
-  });
+chrome.browserAction.onClicked.addListener(function(tab) {
+  var host = activeTabs[tab.id];
+  if (host) {
+    activeHost[host] = false;
+    delete activeTabs[tab.id];
+    stopExt(tab.id);
+  } else {
+    host = getHost(tab.url);
+    activeTabs[tab.id] = host;
+    activeHost[host] = true;
+    startExt(tab.id);
+  }
+  localStorage.activeHost = JSON.stringify(activeHost);
+});
 
-  chrome.browserAction.onClicked.addListener(function(tab) {
-    if (activeTabs[tab.id]) {
-      inactiveHost.push(activeTabs[tab.id]);
-      delete activeTabs[tab.id];
-      stopExt(tab.id);
-    } else {
-      var host = getHost(tab.url);
-      activeTabs[tab.id] = host;
-      var i = inactiveHost.indexOf(host);
-      if (i)
-        inactiveHost.splice(i,1);
-      startExt(tab.id);
-    }
-  });
-
-  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
-    if (changeInfo.status == "loading") {
-      if (! changeInfo.url)
-        return;
-      var host = getHost(changeInfo.url);
-      if (localStorage[host] && inactiveHost.indexOf(host) == -1)
-        activeTabs[tabId] = host;
-      else
-        delete activeTabs[tabId];
-    } else {
-      if (activeTabs[tabId])
-        startExt(tabId);
-    }
-  });
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
+  if (changeInfo.status == "loading") {
+    if (! changeInfo.url)
+      return;
+    var host = getHost(changeInfo.url);
+    if (activeHost[host])
+      activeTabs[tabId] = host;
+    else
+      delete activeTabs[tabId];
+  } else if (changeInfo.status == "complete" && activeTabs[tabId])
+    startExt(tabId);
 });
 
 function startExt(tabId) {
-  var msg = {dest: 'contentscript', action: 'start'};
-  chrome.tabs.sendMessage(tabId, msg);
+  chrome.tabs.sendMessage(tabId, {dest: 'contentscript', action: 'start'});
 };
 
 function stopExt(tabId) {
-  var msg = {dest: 'contentscript', action: 'stop'};
-  chrome.tabs.sendMessage(tabId, msg);
+  chrome.tabs.sendMessage(tabId, {dest: 'contentscript', action: 'stop'});
 };
 
 function getHost(url) {
   var res = url.match(/:\/\/[^\/]+\.\w+\//)[0].slice(3,-1);
   return res;
 };
-//  "content_security_policy": "default-src 'none'; style-src 'self'; script-src 'self'; connect-src https://maps.googleapis.com; img-src https://maps.google.com"
