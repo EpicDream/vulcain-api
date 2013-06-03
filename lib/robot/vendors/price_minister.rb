e0d0513cfc3d798caab30a071db504fe4a148cee
anoiaque <anoiaque@gmail.com>
2013-06-03 12:53:11 +0200

    Price Minister strategy
    - without step fill address

---
+++ lib/robot/vendors/price_minister.rb
@@ -0,0 +1,155 @@
+class PriceMinister
+  USER_AGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3"
+  PRICES_IN_TEXT = lambda do |text| 
+    text.scan(/[\d,]+\s*€/).flatten.map { |price| price.gsub(',', '.').to_f }
+  end
+  
+  URL = 'http://www.priceminister.com'
+  
+  LOGIN_URL = 'https://www.priceminister.com/connect'
+  ADD_TO_CART = '//*[@id="advert_585244223"]/ul/li[5]/form/div/input'
+  
+  
+  attr_accessor :context, :robot
+  
+  def initialize context
+    @context = context.merge!({options:{user_agent:USER_AGENT}})
+    @robot = instanciate_robot
+  end
+  
+  def instanciate_robot
+    Robot.new(@context) do
+
+      step('run') do
+        if account.new_account
+          run_step 'create account'
+        else
+          run_step 'renew login'
+        end
+      end
+      
+      step('renew login') do
+        run_step('logout')
+        open_url order.products_urls[0] #affiliation cookie
+        run_step('login')
+      end
+      
+      step('logout') do
+      end
+      
+      step('login') do
+        open_url URL
+        open_url LOGIN_URL
+        fill_element_with_attribute_matching("input", "id", /user_email/, with:account.login)
+        fill_element_with_attribute_matching("input", "id", /user_pwd/, with:account.password)
+        move_to_and_click_on '//div[@class="pm_action"]/div'
+        message :logged, :next_step => 'empty cart'
+      end
+      
+      step('build product') do
+        product = Hash.new
+        product['price_text'] = get_text '//span[@class="value price"]'
+        product['product_title'] = get_text '//h1[@class="product_title"]'
+        product['product_image_url'] = ''
+        prices = PRICES_IN_TEXT.(product['price_text'])
+        product['price_product'] = prices[0]
+        product['url'] = current_product_url
+        puts product.inspect
+        products << product
+      end
+      
+      step('add to cart') do
+        if url = next_product_url
+          open_url url
+          
+          found = wait_for ['//form[@class="pm_frm"]'] do
+            message :no_product_available
+            terminate_on_error(:no_product_available) 
+          end
+          
+          if found
+            run_step('build product')
+            move_to_and_click_on '//form[@class="pm_frm"]/div'
+            wait_ajax(3)
+            run_step 'add to cart'
+          end
+        else
+          message :cart_filled, :next_step => 'finalize order'
+        end
+      end
+      
+      step('empty cart') do |args|
+        message :cart_emptied, :next_step => 'add to cart'
+      end
+      
+      step('fill shipping form') do
+      end
+      
+      step('finalize order') do
+        open_url "http://www.priceminister.com/cart"
+        open_url "https://www.priceminister.com/checkout/address"
+        
+        wait_for(['//div[@class="pm_action"]'])
+        move_to_and_click_on '//div[@class="pm_action"]/div'
+        run_step('submit credit card')
+      end
+      
+      step('submit credit card') do
+        run_step('build final billing')
+        wait_ajax 5
+        if exists? '//div[@class="ui-select"]//select[@name="cardType"]'
+          select_option('//div[@class="ui-select"]//select[@name="cardType"]', "VISA")
+          fill '//*[@id="cardNumber"]', with:order.credentials.number
+          select_option '//select[@name="expMonth"]', order.credentials.exp_month.to_s
+          select_option '//select[@name="expYear"]', order.credentials.exp_year.to_s
+          fill '//*[@id="securityCode"]', with:order.credentials.cvv
+        end
+        assess
+      end
+      
+      step('cancel') do
+        terminate_on_cancel
+      end
+      
+      step('payment') do
+        answer = answers.last
+        action = questions[answers.last.question_id]
+
+        if eval(action)
+          run_step('validate order')
+        else
+          open_url URL
+          run_step('empty cart', next_step:'cancel')
+        end
+      end
+      
+      step('build final billing') do
+        wait_ajax 3
+        price_text = get_text '//p[@class="total_sum"]'
+        product = PRICES_IN_TEXT.(price_text)[0]
+        self.billing = { product:product, shipping:0, total:product}
+      end
+      
+      step('remove credit card') do
+      end
+      
+      step('validate order') do
+        screenshot
+        page_source
+        move_to_and_click_on '//div[@class="pm_action btn_checkout"]/div'
+        wait_for(['//div[@class="notification success"]'])
+        if exists?('//div[@class="notification success"]')
+          run_step('remove credit card')
+          terminate({ billing:self.billing})
+        else
+          run_step('remove credit card')
+          terminate_on_error(:order_validation_failed)
+        end
+      end
+      
+      
+    end
+    
+  end
+end
+
---
+++ test/unit/robot/price_minister_test.rb
@@ -0,0 +1,105 @@
+require 'test_helper'
+require_robot 'price_minister'
+
+class RueDuCommerceTest < ActiveSupport::TestCase
+  PRODUCT_1_URL = "http://track.effiliation.com/servlet/effi.redir?id_compteur=11283855&url=http://www.priceminister.com/offer/buy/107408551/sort1/filter10/"
+  
+  attr_accessor :robot
+  
+  setup do
+    @context = {'account' => {'login' => 'marie_rose_18@yopmail.com', 'password' => 'shopelia2013'},
+                'session' => {'uuid' => '0129801H', 'callback_url' => 'http://', 'state' => 'dzjdzj2102901'},
+                'order' => {'products_urls' => [PRODUCT_1_URL],
+                            'credentials' => {
+                              'holder' => 'MARIE ROSE', 
+                              'number' => '101290129019201', 
+                              'exp_month' => 5,
+                              'exp_year' => 2014,
+                              'cvv' => 123}},
+                'user' => {'birthdate' => {'day' => 1, 'month' => 4, 'year' => 1985},
+                           'mobile_phone' => '0134562345',
+                           'land_phone' => '0134562345',
+                           'first_name' => 'Pierre',
+                           'gender' => 1,
+                           'last_name' => 'Legrand',
+                           'address' => { 'address_1' => '12 rue des lilas',
+                                          'address_2' => '',
+                                          'additionnal_address' => '',
+                                          'zip' => '75019',
+                                          'city' => 'Paris',
+                                          'country' => 'France'}
+                          }
+                }
+                
+    @robot = PriceMinister.new(@context).robot
+    @message = stub
+    @robot.messager = stub(:logging => @message, :dispatcher => @message, :vulcain => @message, :admin => @message)
+  end
+  
+  teardown do
+    #@robot.driver.quit
+  end
+  
+  test "login" do
+    @message.expects(:message).times(20)
+    robot.expects(:message).with(:logged, :next_step => 'empty cart')
+
+    robot.run_step('run')
+  end
+  
+  test "logout" do
+  end
+  
+  test "add to cart and finalize order" do
+    @message.expects(:message).times(20)
+    robot.run_step('login')
+    robot.run_step('empty cart')
+    robot.run_step('add to cart')
+
+    # products = [{'price_text' => "28€99\nquantité : 1\ncoût total : 28€99", 'product_title' => "KINGSTON\nBarrettes mémoire portable Kingston So-DIMM DDR3 PC3-12800 - 4 Go - 1600 MHz - CAS 11", 'product_image_url' => 'http://s3.static69.com/composant/images/produits/info/small/KVR400X64SC3A_256__new.jpg', 'price_product' => 31.28, 'price_delivery' => 5.9, 'url' => 'http://m.rueducommerce.fr/fiche-produit/KVR16S11S8%252F4'}]
+    # billing = {:product => 31.28, :shipping => 5.9, :total => 37.18, :shipping_info => 'Date de livraison estimée : entre le 25/05/2013 et le 28/05/2013 par Colissimo suivi'}
+    # questions = [{:text => nil, :id => '1', :options => nil}]
+    # @message.expects(:message).with(:assess, {:questions => questions, :products => products, :billing => billing})
+    
+    robot.run_step('finalize order')
+    
+    # assert_equal products, robot.products
+    # assert_equal billing, robot.billing
+  end  
+  
+  test "with REAL PAYMENT MODE" do
+    @message.expects(:message).times(22)
+    
+    @context = {'account' => {'login' => 'elarch.gmail.com@shopelia.fr', 'password' => '625f508b'},
+                     'session' => {'uuid' => '0129801H', 'callback_url' => 'http://', 'state' => 'dzjdzj2102901'},
+                     'order' => {'products_urls' => [PRODUCT_1_URL],
+                                 'credentials' => {
+                                   'holder' => 'M ERICE LARCHEVEQUE', 
+                                   'number' => '4561003435926735', 
+                                   'exp_month' => 5,
+                                   'exp_year' => 2015,
+                                   'cvv' => 642}},#642
+                     'user' => {'birthdate' => {'day' => 1, 'month' => 4, 'year' => 1985},
+                                'mobile_phone' => '0659497434',
+                                'land_phone' => '0959497434',
+                                'first_name' => 'Eric',
+                                'gender' => 1,
+                                'last_name' => 'Larcheveque',
+                                'address' => { 'address_1' => '14 boulevard du Chateau',
+                                               'address_2' => '',
+                                               'additionnal_address' => '',
+                                               'zip' => '92200',
+                                               'city' => ' Neuilly sur Seine',
+                                               'country' => 'France'}
+                               }
+                     }
+    
+    robot.context = @context
+    robot.run_step('login')
+    robot.run_step('empty cart')
+    robot.run_step('add to cart')
+    robot.run_step('finalize order')
+    robot.run_step('validate order')
+  end
+  
+end
