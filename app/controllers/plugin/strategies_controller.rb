841484b4ab52c8ac09db2fdc08c374c7ad194d0d
Vincent Renaudineau <vincent.renaudineau@prixing.fr>
2013-06-11 12:28:26 +0200

    [Plugin] Add some default actions

--- app/controllers/plugin/strategies_controller.rb
+++ app/controllers/plugin/strategies_controller.rb
@@ -70,34 +70,42 @@ class Plugin::StrategiesController < ApplicationController
             id: "account_creation",
             desc: "Inscription",
             actions: [
+              {id: "go_to_account", desc: "Aller sur la page d'inscription", option: "", type: "pl_open_url"},
               {id: "account_btn", desc: "Mon Compte", option: "", type: "pl_click_on"},
               {id: "email_field", desc: "E-mail", option: "", type: "pl_fill_text","arg"=>"email"},
+              {id: "continuerBtn", desc: "Bouton continuer", option: "", type: "pl_click_on"},
+              {id: "confirm_email_field", desc: "Confirmer l'e-mail", option: "", type: "pl_fill_text","arg"=>"email"},
               {id: "pseudo_field", desc: "Pseudo", option: "", type: "pl_fill_text","arg"=>"login"},
               {id: "password_field", desc: "Mot de passe", option: "", type: "pl_fill_text","arg"=>"password"},
+              {id: "confirm_password_field", desc: "Confirmer le mot de passe", option: "", type: "pl_fill_text","arg"=>"password"},
               {id: "civilite_select", desc: "Civilité", option: "", type: "pl_select_option","arg"=>"gender"},
               {id: "name_field", desc: "Nom", option: "", type: "pl_fill_text","arg"=>"last_name"},
               {id: "prenom_field", desc: "Prénom", option: "", type: "pl_fill_text","arg"=>"first_name"},
               {id: "jourbirth_select", desc: "Jour de Naissance", option: "", type: "pl_select_option","arg"=>"birthdate_day"},
               {id: "moisbirth_select", desc: "Mois de naissance", option: "", type: "pl_select_option","arg"=>"birthdate_month"},
               {id: "anneeBirth_select", desc: "Année de naissance", option: "", type: "pl_select_option","arg"=>"birthdate_year"},
-              {id: "create_btn", desc: "Bouton créer le compte", option: "", type: "pl_click_on"}
+              {id: "telephoneFixe_field", desc: "Télephone fixe", option: "", type: "pl_fill_text","arg"=>"land_phone"},
+              {id: "telephoneMobile_field", desc: "Téléphone mobile", option: "", type: "pl_fill_text","arg"=>"mobile_phone"},
+              {id: "create_btn", desc: "Bouton créer le compte", option: "", type: "pl_click_on"},
+              {id: "check_account_created", desc: "Vérifier que le compte est créé", option: "", type: "pl_check"}
             ]
           },{
             id: "login",
             desc: "Se Connecter",
             actions: [
-                {id: "account_btn", desc: "Mon Compte", option: "", type: "pl_click_on"},
-                {id: "login_field", desc: "Login", option: "", type: "pl_fill_text","arg"=>"login"},
-                {id: "email_field", desc: "E-mail", option: "", type: "pl_fill_text","arg"=>"email"},
-                {id: "password_field", desc: "Mot de passe", option: "", type: "pl_fill_text","arg"=>"password"},
-                {id: "continuerBtn", desc: "Bouton continuer", option: "", type: "pl_click_on"}
+              {id: "account_btn", desc: "Mon Compte", option: "", type: "pl_click_on"},
+              {id: "login_field", desc: "Login", option: "", type: "pl_fill_text","arg"=>"login"},
+              {id: "email_field", desc: "E-mail", option: "", type: "pl_fill_text","arg"=>"email"},
+              {id: "password_field", desc: "Mot de passe", option: "", type: "pl_fill_text","arg"=>"password"},
+              {id: "continuerBtn", desc: "Bouton continuer", option: "", type: "pl_click_on"},
+              {id: "check_logged", desc: "Vérifier qu'on est connecté", option: "", type: "pl_check"}
             ]
           },{
             id: "unlog",
             desc: "Déconnexion",
             actions: [
-                {id: "account_btn", desc: "Mon Compte", option: "", type: "pl_click_on"},
-                {id: "unconnect_btn", desc: "Bouton déconnexion", option: "", type: "pl_click_on"}
+              {id: "account_btn", desc: "Mon Compte", option: "", type: "pl_click_on"},
+              {id: "unconnect_btn", desc: "Bouton déconnexion", option: "", type: "pl_click_on"}
             ]
           },{
             id: "empty_cart",
@@ -105,7 +113,8 @@ class Plugin::StrategiesController < ApplicationController
             actions: [
               {id: "mon_panier_btn", desc: "Bouton mon panier", option: "", type: "pl_click_on"},
               {id: "empty_btn", desc: "Bouton vider le panier", option: "", type: "pl_click_on"},
-              {id: "remove_btn", desc: "Bouton supprimer produit du panier", option: "", type: "pl_click_on_all"}
+              {id: "remove_btn", desc: "Bouton supprimer produit du panier", option: "", type: "pl_click_on_all"},
+              {id: "check_cart_empty", desc: "Vérifier que le panier est vide", option: "", type: "pl_check"}
             ]
           },{
             id: "add_to_cart",
@@ -122,6 +131,9 @@ class Plugin::StrategiesController < ApplicationController
             desc: "Finalisation",
             actions: [
               {id: "mon_panier_btn", desc: "Bouton mon panier", option: "", type: "pl_click_on"},
+              {id: "set_tot_products_price", desc: "Indiquer le prix total des produits", option: "", type: "pl_set_tot_products_price"},
+              {id: "set_tot_shipping_price", desc: "Indiquer le prix de livraison total", option: "", type: "pl_set_tot_shipping_price"},
+              {id: "set_total_price", desc: "Indiquer le prix total", option: "", type: "pl_set_total_price"},
               {id: "finalize_btn", desc: "Bouton finalisation", option: "", type: "pl_click_on"},
               {id: "civilite_select", desc: "Civilité", option: "", type: "pl_select_option","arg"=>"gender"},
               {id: "name_field", desc: "Nom", option: "", type: "pl_fill_text","arg"=>"last_name"},
@@ -142,7 +154,8 @@ class Plugin::StrategiesController < ApplicationController
               {id: "cvc", desc: "CVC", option: "", type: "pl_fill_text"},
               {id: "expire_month", desc: "Mois d'expiration", option: "", type: "pl_select_option"},
               {id: "expire_year", desc: "Année d'expiration", option: "", type: "pl_select_option"},
-              {id: "continuer_btn", desc: "Bouton Continuer", option: "", type: "pl_click_on"}
+              {id: "continuer_btn", desc: "Bouton Continuer", option: "", type: "pl_click_on"},
+              {id: "check_payment", desc: "Vérifier que la transaction est passée", option: "", type: "pl_check"}
             ]
           }
         ]
