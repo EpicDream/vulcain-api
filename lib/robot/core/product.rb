8385bfd21f60d6972bf9cbc7f4c07b6a27c3faaf
anoiaque <anoiaque@gmail.com>
2013-09-02 16:48:36 +0200

    * insert price_delivery in product hash infos.

--- lib/robot/core/product.rb
+++ lib/robot/core/product.rb
@@ -7,6 +7,7 @@ module RobotCore
       product['product_title'] = robot.get_text vendor::PRODUCT[:title]
       product['product_image_url'] = robot.image_url vendor::PRODUCT[:image]
       product['price_product'] = PRICES_IN_TEXT.(product['price_text']).first
+      product['price_delivery'] = PRICES_IN_TEXT.(robot.get_text vendor::PRODUCT[:shipping]).first
       product['url'] = robot.current_product.url
       product['id'] = robot.current_product.id
       robot.products << product
