f718bbd1fa4295acd556b37550dd23189da33e92
anoiaque <anoiaque@gmail.com>
2013-09-02 16:47:33 +0200

    * Handle options(color/size) with selects

--- lib/robot/core/options.rb
+++ lib/robot/core/options.rb
@@ -15,13 +15,23 @@ module RobotCore
     private
     
     def color
-      xpath = vendor::CART[:color_option].gsub(/color_option_value/, product.color)
-      robot.click_on(xpath)
+      node = robot.find_element vendor::CART[:color_option]
+      if node.tag_name == 'select'
+        robot.select_option(vendor::CART[:color_option], product.color)
+      else #image
+        xpath = vendor::CART[:color_option].gsub(/color_option_value/, product.color)
+        robot.click_on(xpath)
+      end
     end
     
     def size
-      xpath = vendor::CART[:size_option].gsub(/size_option_value/, product.size)
-      robot.click_on(xpath)
+      node = robot.find_element vendor::CART[:size_option]
+      if node.tag_name == 'select'
+        robot.select_option(vendor::CART[:size_option], product.size)
+      else #image
+        xpath = vendor::CART[:size_option].gsub(/size_option_value/, product.size)
+        robot.click_on(xpath)
+      end
     end
     
   end
