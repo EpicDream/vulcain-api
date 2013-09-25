4872a33a84994c228897eacfbabebea7ffac0fda
anoiaque <anoiaque@gmail.com>
2013-09-25 16:11:22 +0200

    * match first 10 chars if no match with all string

--- lib/robot/core/quantities.rb
+++ lib/robot/core/quantities.rb
@@ -24,17 +24,23 @@ module RobotCore
     def line_and_index_of product
       lines = RobotCore::CartLine.all
       index = -1
+      title_1 = product['product_title'].gsub(/\n/, ' ')
 
-      return [lines.first, 0] if products.count.zero?
+      return [lines.first, 0] if products.count == 1
       line = lines.detect { |line|  
-        title_1 = product['product_title'].gsub(/\n/, ' ')
         title_2 = line.title.gsub(/\n/, ' ')
-        match = title_1 =~ Regexp.new(Regexp.escape(title_2), Regexp::IGNORECASE)
-        match ||= title_2 =~ Regexp.new(Regexp.escape(title_1), Regexp::IGNORECASE)
+        match = title_1 =~ regexp_from(title_2)
+        match ||= title_2 =~ regexp_from(title_1)
+        match ||= title_1 =~ regexp_from(title_2, 0..10)
+        match ||= title_2 =~ regexp_from(title_1, 0..10)
         index += 1
         match
       }
       [line, index]
+    end
+    
+    def regexp_from title, range=(0..-1)
+      Regexp.new(Regexp.escape(title[range]), Regexp::IGNORECASE)
     end
     
     def quantities_cannot_be_set?
