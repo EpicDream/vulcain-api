f58af8bafc6e0f29de24d369d6de625f833daf09
anoiaque <anoiaque@gmail.com>
2013-10-04 18:12:40 +0200

    clean

--- lib/robot/vendors/super_insolite.rb
+++ lib/robot/vendors/super_insolite.rb
@@ -93,37 +93,8 @@ module SuperInsoliteConstants
   
 end
 
-module SuperInsoliteCrawler
-  class ProductCrawler
-    
-    attr_reader :product
-    
-    def initialize robot, xpaths
-      @robot = robot
-      @xpaths = xpaths
-      @product = {:options => {}}
-    end
-    
-    def crawl url
-      @url = url
-      @robot.open_url url
-      @page = Nokogiri::HTML.parse @robot.driver.page_source
-      build_options
-      build_product
-    end
-    
-    def build_options
-    end
-    
-    def build_product
-    end
-    
-  end
-end
-
 class SuperInsolite
   include SuperInsoliteConstants
-  include SuperInsoliteCrawler
   
   attr_accessor :context, :robot
   
