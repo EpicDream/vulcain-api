43334f61bd95b9f415d5ee1165d3c4bfd7582ad0
anoiaque <anoiaque@gmail.com>
2013-10-07 11:31:03 +0200

    * clear and wait. ==Don't think it will fix the problem with filling inputs

--- lib/robot/core/actions.rb
+++ lib/robot/core/actions.rb
@@ -119,12 +119,12 @@ module RobotCore
       end
       input = identifier if identifier.is_a?(Selenium::WebDriver::Element)
       input ||= @driver.find_element(identifier)
-      input.clear
       10.times do |n|
+        input.clear
+        wait_ajax 0.5
         input.send_key args[:with].to_s
         break if input["value"] == args[:with].to_s
         terminate_on_error(:fill_input_error) if n == 9
-        wait_ajax 0.5
       end
     end
     
