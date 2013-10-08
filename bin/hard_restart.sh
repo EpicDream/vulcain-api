04fd37544f91fa7525279128a369fa283cf483b4
anoiaque <anoiaque@gmail.com>
2013-10-08 16:56:48 +0200

    echo what is done

--- bin/hard_restart.sh
+++ bin/hard_restart.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+echo 'Stop dispatcher ...'
 /etc/init.d/vulcain-dispatcher stop
-kill $(ps aux | pgrep -f 'vulcain/bin/run.rb')
+
+echo 'Kill(SIGINT) Vulcains Processes ...'
+kill -2 $(ps aux | pgrep -f 'vulcain/bin/run.rb')
+
+echo 'Start Dispatcher ...'
 /etc/init.d/vulcain-dispatcher start
+
+echo 'Re-start Unicorns ...'
 /etc/init.d/vulcain-unicorn restart
 
