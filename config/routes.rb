ec71e9b998450b4a2e67a0e82944856b3f49aaea
anoiaque <anoiaque@gmail.com>
2013-10-09 14:51:04 +0200

    clean

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 VulcainApi::Application.routes.draw do
-  # PLUGIN
-  get "plugin/strategies/actions"
-  post "plugin/strategies/create"
-  get "plugin/strategies/show"
-  post "plugin/strategies/test"
 
   resource :orders, :only => [:create]
   resource :answers, :only => [:create]
-  resource :product_informations, :only => [:create]
   
   namespace :admin do
     resources :monitors, :only => [:index, :show, :create]
