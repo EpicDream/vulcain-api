f362f9c0bfaa148bd6acd93d1351743fde6f4159
anoiaque <anoiaque@gmail.com>
2013-10-09 15:11:47 +0200

    * Ask for confirmation

--- bin/hard_restart.sh
+++ bin/hard_restart.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
+function stopDispatcher {
+  echo 'Stop dispatcher ...'
+  /etc/init.d/vulcain-dispatcher stop
+}
 
-echo 'Stop dispatcher ...'
-/etc/init.d/vulcain-dispatcher stop
+function killVulcains {
+  echo 'Kill(SIGINT) Vulcains Processes ...'
+  kill -2 $(ps aux | pgrep -f 'vulcain/bin/run.rb')
+}
 
-echo 'Kill(SIGINT) Vulcains Processes ...'
-kill -2 $(ps aux | pgrep -f 'vulcain/bin/run.rb')
+function startDispatcher {
+  echo 'Start Dispatcher ...'
+  /etc/init.d/vulcain-dispatcher start
+}
 
-echo 'Start Dispatcher ...'
-/etc/init.d/vulcain-dispatcher start
+function restartUnicorns {
+  echo 'Re-start Unicorns ...'
+  /etc/init.d/vulcain-unicorn restart
+}
 
-echo 'Re-start Unicorns ...'
-/etc/init.d/vulcain-unicorn restart
+echo 'WARNING. This script will kill all vulcains processes. Be sure no one is running.'
+
+read -p 'Continue?(y/n)' choice
+
+if [ $choice = "y" ]; then
+  stopDispatcher
+  killVulcains
+  startDispatcher
+  restartUnicorns
+else
+  exit
+fi  
+
+
+
 
