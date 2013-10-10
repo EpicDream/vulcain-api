70377d2ddf91dcebdde25afb31655dfa823b5d9a
anoiaque <anoiaque@gmail.com>
2013-10-10 16:14:36 +0200

    * Clean

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -28,17 +28,6 @@ module ActiveSupportTestCaseExtension
 end
 
 class ActiveSupport::TestCase
-  VCR.configure do |c|
-    c.cassette_library_dir = "#{Rails.root}/test/fixtures/cassettes"
-    c.hook_into :webmock
-    c.ignore_localhost = true
-    c.default_cassette_options = {
-      :record => :new_episodes,
-      :serialize_with => :syck,
-      :match_requests_on => [:method, :body]
-    }
-    c.allow_http_connections_when_no_cassette = true
-  end
   
   def teardown
     MongoMapper.database.collections.each do |coll|
