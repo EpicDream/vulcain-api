8b5defc3159b2c1248a6ec53c0b972f97161f5db
anoiaque <anoiaque@gmail.com>
2013-11-05 13:13:47 +0100

    *clean

--- lib/robot/core_extensions.rb
+++ lib/robot/core_extensions.rb
@@ -269,48 +269,6 @@ class Object
   end
 end
 
-module Enumerable
-  # Allow to pass method and args without block.
-  # Ex : [1,2,3,4].map_send(:+,1) => [2,3,4,5]
-  def map_send(*args)
-    map { |x| x.send(*args) }
-  end
-
-  # Delete one obj and return it, or nil if nothing found.
-  def delete_one(obj=nil, &block)
-    if obj
-      return self.slice!(self.index(obj))
-    elsif block
-      self.each_with_index do |e,i|
-        return self.slice!(i) if yield e
-      end
-    end
-    nil
-  end
-  # Delete one obj and return the Enumerable
-  def delete_one!(obj=nil, &block)
-    if obj
-      self.slice!(self.index(obj))
-    elsif block
-      self.each_with_index do |e,i|
-        if yield e
-          self.slice!(i)
-          break
-        end
-      end
-    end
-    self
-  end
-  # Replace in place by arg if block return true.
-  # [1,2,3,4,5,6].replace_by_if(0) { |n| n.even? } => [1,0,3,0,5,0]
-  def replace_by_if(arg, &block)
-    raise ArgumentError unless block
-    self.each_with_index do |e,i|
-      self[i] = arg if yield e
-    end
-  end
-end
-
 class String
   UNACCENT_HASH = {
     'A'   => 'ÀÁÂÃÄÅĀĂǍẠẢẤẦẨẪẬẮẰẲẴẶǺĄ',
@@ -364,18 +322,5 @@ class String
       _str.gsub!(/[#{v}]/, k)
     end
     _str
-  end
-end
-
-class OpenStruct
-  # instead of obj.key,
-  # can also do obj[:key] or obj["key"].
-  def [](name)
-    self.send(name) if self.methods.include?((name.to_s+"=").to_sym) rescue nil
-  end
-  # instead of obj.key = value,
-  # can also do obj[:key] = value or obj["key"] = value.
-  def []=(name, value)
-    self.send((name.to_s+"=").to_sym, value) rescue nil
   end
 end
