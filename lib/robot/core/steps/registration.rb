1d143d59a1f44110c5cdd2afc87b7bfce13585ee
anoiaque <anoiaque@gmail.com>
2013-11-06 17:08:37 +0100

    * Registration step

--- lib/robot/core/steps/registration.rb
+++ lib/robot/core/steps/registration.rb
@@ -1,9 +1,16 @@
 module Robot
   module Step
     class Registration
-      def initialize
-        
+
+      def initialize context
+        @context = context
       end
+      
+      def run
+        Action(:open_url, :register)
+        Action(:wait_for, [:submit_login, :submit])
+      end
+      
     end
   end
 end
