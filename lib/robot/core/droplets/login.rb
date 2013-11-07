27d5bd167536c757b6697667213e848f0a64e289
anoiaque <anoiaque@gmail.com>
2013-11-07 17:54:27 +0100

    * Address and Login droplets

--- lib/robot/core/droplets/login.rb
+++ lib/robot/core/droplets/login.rb
@@ -1,7 +1,22 @@
 module Robot
   module Droplet
+
     class Login
+      include Robot::ActionWrapper
+      
+      def initialize context, dictionary
+        @account = context.account
+        @dictionary = dictionary
+      end
+      
+      def run
+        action :fill, :email, with:@account.login
+        action :fill, :email_confirmation, with:@account.login
+        action :fill, :password, with:@account.password
+        action :fill, :password_confirmation, with:@account.password
+      end
       
     end
+    
   end
 end
