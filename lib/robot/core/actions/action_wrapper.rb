3391f09af2b96fdb5cea7150c6c6f4a293bfd42a
anoiaque <anoiaque@gmail.com>
2013-11-07 17:53:37 +0100

    * ActionWrapper handle flow == ie , registration can be done with 2 page flows (login enter = submit login = address form == submit)

--- lib/robot/core/actions/action_wrapper.rb
+++ lib/robot/core/actions/action_wrapper.rb
@@ -5,22 +5,42 @@ module Robot
       Object.const_get(@context.vendor.to_s).const_get(name)
     end
     
-    def action action, key=nil, opts={}, &block
-      identifier = identifier_from(key)
-      
-      if block_given?
-        Robot::Action.send(action, identifier, opts, &block)
-      else
-        Robot::Action.send(action, identifier, opts)
-      end
+    def nexts
+      @nexts ||= @dictionary.keys.select { |key| key.to_s =~ /^next\d$/}.sort
     end
     
-    def identifier_from key
-      case key
-      when Array
-        key.map { |k| @dictionary[k] }
+    def next_flow
+      return if @nexts.count < 2
+      _next = nexts.shift
+      action :click_on, _next
+    end
+    
+    def wait_flow
+      action :wait_for, nexts.first
+    end
+    
+    def continue?
+      wait_flow rescue nil
+    end
+    
+    def error
+      { error:failure_code }
+    end
+    
+    def submit
+      action :click_on, nexts.last
+      @success = action(:wait_leave, nexts.last)
+    end
+    
+    def status
+      @success ? {success:success_code} : error()
+    end
+    
+    def action action, key=nil, opts={}, &block
+      if block_given?
+        Robot::Action.send(action, @dictionary[key], opts, &block)
       else
-        key.is_a?(Symbol) ? @dictionary[key] : key
+        Robot::Action.send(action, @dictionary[key], opts)
       end
     end
     
